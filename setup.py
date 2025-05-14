08620c0eab94375a50cdbf52484075d204743c4a
Aidan Curtis <aidancurtis@dhcp-10-29-217-223.dyn.MIT.EDU>
2025-05-14 15:38:19 -0400

    try again

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 from setuptools import setup
-from setuptools.command.build_py import build_py as _build_py
+from setuptools.command.build_ext import build_ext as _build_ext
 import subprocess
 import os
 
 
-class CustomBuildPy(_build_py):
+class CustomBuildExt(_build_ext):
     def run(self):
-        # Run the original build_py first
-        super().run()
-
-        # Now run your custom script
-        symk_dir = os.path.join(os.path.dirname(__file__), "tampura/third_party", "symk")
+        symk_dir = os.path.join(os.path.dirname(__file__), "tampura", "third_party", "symk")
         subprocess.check_call(["python", "build.py"], cwd=symk_dir)
+        super().run()
 
 
 setup(
-    name="tampura",
-    version="0.1.0",
-    packages=["tampura"],
     cmdclass={
-        'build_py': CustomBuildPy,
-    },
+        "build_ext": CustomBuildExt,
+    }
 )
